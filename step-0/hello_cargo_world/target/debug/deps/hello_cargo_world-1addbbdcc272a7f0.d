b432f46c5e052494c750de0b764b55507a20bff8
kjayantmenon <kjayantmenon@gmail.com>
2022-06-24 02:02:19 +0000

    removed target folder

--- step-0/hello_cargo_world/target/debug/deps/hello_cargo_world-1addbbdcc272a7f0.d
+++
@@ -1,5 +0,0 @@
-/home/jayant/spikes/rust/rust-tales/step-0/hello_cargo_world/target/debug/deps/hello_cargo_world-1addbbdcc272a7f0: src/main.rs
-
-/home/jayant/spikes/rust/rust-tales/step-0/hello_cargo_world/target/debug/deps/hello_cargo_world-1addbbdcc272a7f0.d: src/main.rs
-
-src/main.rs:
--- step-0/hello_cargo_world/target/debug/hello_cargo_world.d
+++
@@ -1,1 +0,0 @@
-/home/jayant/spikes/rust/rust-tales/step-0/hello_cargo_world/target/debug/hello_cargo_world: /home/jayant/spikes/rust/rust-tales/step-0/hello_cargo_world/src/main.rs
