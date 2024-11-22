8fadb7c8ced335d2bc959d5ee49af2a4bfc4bd08
Jayant <jayant@Jayants-MacBook-Pro.local>
2024-11-22 20:31:25 +0530

    added details related to mut keyword

--- snake-game/src/main.rs
+++ snake-game/src/main.rs
@@ -1,4 +1,10 @@
 fn main(){
     let greeting = "Hello, World!";
     println!("{}", greeting);
+
+    let mut counter = 0;
+    println!{"{}", counter};
+    counter = counter + 1;
+    
+    println!{"{}", counter};
 }
